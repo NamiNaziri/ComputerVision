e42123441ef66fd36697a826830e0c23b3252038
NamiNaziri <naminaziri1@gmail.com>
2022-03-01 21:36:49 +0330

    Assignment1 completed

--- Assignment1/Assignment1.m
+++ Assignment1/Assignment1.m
@@ -7,8 +7,9 @@ im = imread('Test_01.ppm');
 
 % showing two images 
 
-%subplot(1,2,1),imshow(im,[]),title('1');
-%subplot(1,2,2),imshow(uint8(RGBImageData),[]),title('2');
+figure
+subplot(1,2,1),imshow(im,[]),title('internal loader');
+subplot(1,2,2),imshow(uint8(RGBImageData),[]),title('custom loader');
 
 
 % end of part 1
@@ -18,24 +19,46 @@ im = imread('Test_01.ppm');
 
 specialImage = SpecialEffect(im);
 
+figure
 imshow(specialImage,[]);
-%imwrite(im,'Q2.jpg','Quality',100);
+
 
 % end of part 2
 %=====================================================================
 
-angle = 90;
-
 CustomRotatedImage = CustomRotate(specialImage, 266);
-
 RotatedImage = imrotate(specialImage, 266);
 
+figure
 subplot(1,2,1),imshow(RotatedImage,[]),title('rotated by imrotate');
-subplot(1,2,2),imshow(CustomRotatedImage,[]),title('2');
+subplot(1,2,2),imshow(CustomRotatedImage,[]),title('rotated by custom rotation');
 
-g1 = RotatedImage(:,:,1);
-g2 = CustomRotatedImage(:,:,1);;
 
+
+CustomRotatedImage = CustomRotate(specialImage, 180);
+RotatedImage = imrotate(specialImage, 180);
+
+figure
+subplot(1,2,1),imshow(RotatedImage,[]),title('rotated by imrotate');
+subplot(1,2,2),imshow(CustomRotatedImage,[]),title('rotated by custom rotation');
+
+
+CustomRotatedImage = CustomRotate(specialImage, 30);
+RotatedImage = imrotate(specialImage, 30);
+
+figure
+subplot(1,2,1),imshow(RotatedImage,[]),title('rotated by imrotate');
+subplot(1,2,2),imshow(CustomRotatedImage,[]),title('rotated by custom rotation');
+
+
+
+CustomRotatedImage = CustomRotate(specialImage, 90);
+RotatedImage = imrotate(specialImage, 90);
+
+figure
+subplot(1,2,1),imshow(RotatedImage,[]),title('rotated by imrotate');
+subplot(1,2,2),imshow(CustomRotatedImage,[]),title('rotated by custom rotation');
 %imwrite(uint8(NewRotatedImage),'Q3.png');
 
-
+% end of part 3
+%=====================================================================
