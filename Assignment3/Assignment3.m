83605235de493fdf8b29cfe0d109f8e3e25946aa
NamiNaziri <naminaziri1@gmail.com>
2022-04-08 11:33:19 +0430

    Updated Q3

--- Assignment3/Assignment3.m
+++ Assignment3/Assignment3.m
@@ -2,7 +2,7 @@ clc; close all; clear;
 
 ImageDatasetPath = 'Questions/Q3/';
 
-FeatureDataset = cell(1000,4);
+%FeatureDataset = cell(1000,4);
 
 s = dir(ImageDatasetPath);
 
@@ -20,7 +20,7 @@ for i=1:numel(s)
         M = repmat(all(I<10,3),[1 1 3]);
         I(M) = 255;
         
-        FeatureDataset{k,4} = RGBMid(I);
+        FeatureDataset{k,4} = I;
         
         H = fspecial('Average', [3,3]);
         I = imfilter(I,H);
@@ -37,7 +37,7 @@ for i=1:numel(s)
 end
 %%
 
-ImageSourcePath = 'Questions/Q3Helper/';
+ImageSourcePath = 'Questions/Q3_Source/';
 SourceDataset = cell(20,3);
 
 s = dir(ImageSourcePath);
@@ -49,7 +49,7 @@ for i=1:numel(s)
         SourceDataset{L,1} = s(i).name;
         
         Answer = s(i).name(find(s(i).name == '_', 1, 'last') + 1: end - 4);
-        I = imresize(I,[100,50]);
+        %I = imresize(I,[100,50]);
         SourceDataset{L,2} = I;
         SourceDataset{L,3} = str2num(Answer);
 
@@ -58,13 +58,17 @@ for i=1:numel(s)
 end
 
 %%
+class = 81;
+rightGuesses = 0;
 
 sourceWidth = 100;
 sourceHeight = 100;
 
 threshold1 = 100;
 
-testImage = imcomplement(im2double(FeatureDataset{41,3})) ;
+
+for p=1 : size(FeatureDataset,1)
+testImage = imcomplement(im2double(FeatureDataset{p,3})) ;
 testImage = testImage(8:end - 60,8: end - 10);
 VerticalScan = sum( testImage()) ;
 
@@ -74,62 +78,76 @@ HorizontalScan = (sum( testImage,2 ))' ;
 [yMin,yMax] = findMinMaxArray(HorizontalScan);
 
 
-for i = 1 : min(size(xMin,2),size(xMax,2))
-    for j = 1 : min(size(yMin,2),size(yMax,2))
-        cropedImage = testImage(yMin(j):yMax(j),xMin(i):xMax(i));
-        if sum(cropedImage(:)) > 5
-            VerticalScan = sum( cropedImage()) ;
 
-            [xMin1,xMax1] = findMinMaxArray(VerticalScan);
-            HorizontalScan = (sum( cropedImage,2 ))' ;
+finalAnswer = 0;
 
-            [yMin1,yMax1] = findMinMaxArray(HorizontalScan);
-            
-            cropedImageHeight = size(cropedImage,1);
-            cropedImageWidth = size(cropedImage,2);
-            
-            f = FeatureDataset{41,4}(yMin(j) +6 + yMin1: yMax(j)+6 - (cropedImageHeight - yMax1) ,xMin(i)+6 + xMin1: xMax(i)+6 -(cropedImageWidth - xMax1),:);
-            %f = FeatureDataset{35,4}(yMin(j) +6:yMax(j) + 6,xMin(i) + 6:xMax(i) + 6,:);
-            f = imresize(f,[sourceHeight,sourceWidth]);
-            
-            red = f(:,:,1);
-            blue = f(:,:,3);
-            
-            isPositive = sum(red(:)) > sum(blue(:));
-            
-            minError = 10000000;
-            selectedImage = 1;
-            
-            if( abs (sum(red(:)) - sum(blue(:))) > threshold1)
-                
-               % for l = 1 : 18
-                %    err = immse(rgb2gray(f), rgb2gray(SourceDataset{l,2}));
-                %    if(err < minError)
-                 %       selectedImage = l;
-                 %       minError = err;
-                %    end
-               % end
-               % figure; imshow([f SourceDataset{selectedImage,2}]);
-               imwrite (f,[num2str(i) num2str(j) '.png']);
+
+    for i = 1 : min(size(xMin,2),size(xMax,2))
+        for j = 1 : min(size(yMin,2),size(yMax,2))
+            cropedImage = testImage(yMin(j):yMax(j),xMin(i):xMax(i));
+            if sum(cropedImage(:)) > 5
+                VerticalScan = sum( cropedImage()) ;
+
+                [xMin1,xMax1] = findMinMaxArray(VerticalScan);
+                HorizontalScan = (sum( cropedImage,2 ))' ;
+
+                [yMin1,yMax1] = findMinMaxArray(HorizontalScan);
+
+                cropedImageHeight = size(cropedImage,1);
+                cropedImageWidth = size(cropedImage,2);
+
+                f = FeatureDataset{p,4}(yMin(j) +6 + yMin1: yMax(j)+6 - (cropedImageHeight - yMax1) ,xMin(i)+6 + xMin1: xMax(i)+6 -(cropedImageWidth - xMax1),:);
+                %f = FeatureDataset{35,4}(yMin(j) +6:yMax(j) + 6,xMin(i) + 6:xMax(i) + 6,:);
+                f = imresize(f,[sourceHeight,sourceWidth]);
+
+                red = f(:,:,1);
+                blue = f(:,:,3);
+
+                isPositive = sum(red(:)) > sum(blue(:));
+
+                sign = -1;
+                if isPositive 
+                    sign = 1; 
+                end
+
+
+                minError = 10000000;
+                selectedImage = 1;
+
+                if( abs (sum(red(:)) - sum(blue(:))) > threshold1)
+
+                    for l = 1 : 18
+                        err = immse(rgb2gray(f), rgb2gray(SourceDataset{l,2}));
+                        if(err < minError)
+                            selectedImage = l;
+                            minError = err;
+                        end
+                    end
+                    if( p == class)
+                    figure; imshow([f SourceDataset{selectedImage,2}]);
+                    end
+                    a = sign * SourceDataset{selectedImage,3};
+                    finalAnswer = finalAnswer + a;
+                   %imwrite (f,[num2str(i) num2str(j) '.png']);
+                end
             end
-            
-            
-            
-            
-            
         end
     end
+if FeatureDataset{p,2} == finalAnswer
+    rightGuesses = rightGuesses + 1;
+else
+    p
 end
-
+end
 %%
-I = imread([ImageDatasetPath FeatureDataset{35,1}]);
+%I = imread([ImageDatasetPath FeatureDataset{35,1}]);
 
-M = repmat(all(I<10,3),[1 1 3]);
-I(M) = 255;
+%M = repmat(all(I<10,3),[1 1 3]);
+%I(M) = 255;
 
-I = RGBMid(I);
+%I = RGBMid(I);
 %H = fspecial('gaussian', [2,2],2/6);
-H = fspecial('Average', [2,2]);
-I = imfilter(rgb2gray(I),H); 
-imshow(I)
+%H = fspecial('Average', [2,2]);
+%I = imfilter(rgb2gray(I),H); 
+%imshow(I)
 %im = imread('Untitled.png');
--- Assignment3/RGBMid.m
+++ Assignment3/RGBMid.m
@@ -2,7 +2,7 @@ function out = RGBMid(I)
     R = I(:,:,1);
     G = I(:,:,2);
     B = I(:,:,3);
-    out(:,:,1) = medfilt2(R,[2,2]);
-    out(:,:,2) = medfilt2(G,[2,2]);
-    out(:,:,3) = medfilt2(B,[2,2]);
+    out1(:,:) = medfilt2(R,[2,2]);
+    out2(:,:) = medfilt2(B,[2,2]);
+    out = out1 + out2;
 end
