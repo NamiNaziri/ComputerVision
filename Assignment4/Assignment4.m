ed7e1b209ca76b64444752149eb7679c08f2ffeb
NamiNaziri <naminaziri1@gmail.com>
2022-05-03 01:11:09 +0430

    update.

--- Assignment4/Assignment4.m
+++ Assignment4/Assignment4.m
@@ -24,134 +24,110 @@ notAssignedRight = {NotAssigned.Right};
 
 
 
-PossibleAnswer = struct;
-PossibleAnswerCounter = 1;
 
-for i=1:size(FoundImage,2)
-    row = FoundImage(i).row;
-    Column = FoundImage(i).Column;
+
+
+
+for j=1:size(Dataset,1)
     
-    k = 1;
-    TempPossibleAnswer = struct;
-    if(row-1 > 1) % top pic
-        if(isempty(imageCell{row-1,Column}))
-            %top of this pic and bottom of the other
-            result = CustomNormCalculator(notAssignedBottom,{FoundImage(i).Top});
-            [M,I] = min(cell2mat(result));
-            
-            TempPossibleAnswer(k).rowInFoundImage = row-1;
-            TempPossibleAnswer(k).ColumnInFoundImage = Column; 
-            TempPossibleAnswer(k).NotAssinedIndex = I;
-            TempPossibleAnswer(k).Norm = M;
-            k = k + 1;
-        end
-    end
+    PossibleAnswer = struct;
+    PossibleAnswerCounter = 1;
     
-    if(row + 1 < lastRow) % Bottom pic
-        if(isempty(imageCell{row+1,Column}))
-            %Bottom of this pic and Top of the other
-            result = CustomNormCalculator(notAssignedTop,{FoundImage(i).Bottom});
-            
-            [M,I] = min(cell2mat(result));
-            TempPossibleAnswer(k).rowInFoundImage = row+1;
-            TempPossibleAnswer(k).ColumnInFoundImage = Column; 
-            TempPossibleAnswer(k).NotAssinedIndex = I;
-            TempPossibleAnswer(k).Norm = M;
-            k = k + 1;
+    for i=1:size(FoundImage,2)
+        row = FoundImage(i).row;
+        Column = FoundImage(i).Column;
+
+        k = 1;
+        TempPossibleAnswer = struct;
+        if(row-1 > 1) % top pic
+            if(isempty(imageCell{row-1,Column}))
+                %top of this pic and bottom of the other
+                result = CustomNormCalculator(notAssignedBottom,{FoundImage(i).Top});
+                [M,I] = min(cell2mat(result));
+
+                TempPossibleAnswer(k).rowInFoundImage = row-1;
+                TempPossibleAnswer(k).ColumnInFoundImage = Column; 
+                TempPossibleAnswer(k).NotAssinedIndex = I;
+                TempPossibleAnswer(k).Norm = M;
+                k = k + 1;
+            end
         end
-    end
-    if( Column - 1 > 1) % Left pic
-        if(isempty(imageCell{row,Column-1}))
-            %Left of this pic and Right of the other
-            result = CustomNormCalculator(notAssignedRight,{FoundImage(i).Left});
-            
-            [M,I] = min(cell2mat(result));
-            TempPossibleAnswer(k).rowInFoundImage = row;
-            TempPossibleAnswer(k).ColumnInFoundImage = Column-1; 
-            TempPossibleAnswer(k).NotAssinedIndex = I;
-            TempPossibleAnswer(k).Norm = M;
-            k = k + 1;
+
+        if(row + 1 < lastRow) % Bottom pic
+            if(isempty(imageCell{row+1,Column}))
+                %Bottom of this pic and Top of the other
+                result = CustomNormCalculator(notAssignedTop,{FoundImage(i).Bottom});
+
+                [M,I] = min(cell2mat(result));
+                TempPossibleAnswer(k).rowInFoundImage = row+1;
+                TempPossibleAnswer(k).ColumnInFoundImage = Column; 
+                TempPossibleAnswer(k).NotAssinedIndex = I;
+                TempPossibleAnswer(k).Norm = M;
+                k = k + 1;
+            end
         end
-    end
+        if( Column - 1 > 1) % Left pic
+            if(isempty(imageCell{row,Column-1}))
+                %Left of this pic and Right of the other
+                result = CustomNormCalculator(notAssignedRight,{FoundImage(i).Left});
+
+                [M,I] = min(cell2mat(result));
+                TempPossibleAnswer(k).rowInFoundImage = row;
+                TempPossibleAnswer(k).ColumnInFoundImage = Column-1; 
+                TempPossibleAnswer(k).NotAssinedIndex = I;
+                TempPossibleAnswer(k).Norm = M;
+                k = k + 1;
+            end
+        end
+
+        if (Column + 1 < lastColumn) % Right Pic
+            if(isempty(imageCell{row,Column+1}))
+                %Right of this pic and Left of the other
+                result = CustomNormCalculator(notAssignedLeft,{FoundImage(i).Right});
+
+
+                [M,I] = min(cell2mat(result));
+                TempPossibleAnswer(k).rowInFoundImage = row;
+                TempPossibleAnswer(k).ColumnInFoundImage = Column+1; 
+                TempPossibleAnswer(k).NotAssinedIndex = I;
+                TempPossibleAnswer(k).Norm = M;
+                k = k + 1;
+            end
+        end
         
-    if (Column + 1 < lastColumn) % Right Pic
-        if(isempty(imageCell{row,Column+1}))
-            %Right of this pic and Left of the other
-            result = CustomNormCalculator(notAssignedLeft,{FoundImage(i).Right});
-            
-            
-            [M,I] = min(cell2mat(result));
-            TempPossibleAnswer(k).rowInFoundImage = row;
-            TempPossibleAnswer(k).ColumnInFoundImage = Column+1; 
-            TempPossibleAnswer(k).NotAssinedIndex = I;
-            TempPossibleAnswer(k).Norm = M;
-            k = k + 1;
+        if(k == 1) %no neighbours found
+            continue;
         end
+        %Find the minimum norm for current picture
+        [Cmin,minIdx]=min([TempPossibleAnswer.Norm]);
+
+        PossibleAnswer(PossibleAnswerCounter).rowInFoundImage = TempPossibleAnswer(minIdx).rowInFoundImage;
+        PossibleAnswer(PossibleAnswerCounter).ColumnInFoundImage = TempPossibleAnswer(minIdx).ColumnInFoundImage; 
+        PossibleAnswer(PossibleAnswerCounter).NotAssinedIndex = TempPossibleAnswer(minIdx).NotAssinedIndex;
+        PossibleAnswer(PossibleAnswerCounter).Norm = TempPossibleAnswer(minIdx).Norm;
+        PossibleAnswerCounter = PossibleAnswerCounter + 1;
+
     end
-    
-    %Find the minimum norm for current picture
-    [Cmin,minIdx]=min([TempPossibleAnswer.Norm]);
-    
-    PossibleAnswer(PossibleAnswerCounter).rowInFoundImage = TempPossibleAnswer(minIdx).rowInFoundImage;
-    PossibleAnswer(PossibleAnswerCounter).ColumnInFoundImage = TempPossibleAnswer(minIdx).ColumnInFoundImage; 
-    PossibleAnswer(PossibleAnswerCounter).NotAssinedIndex = TempPossibleAnswer(minIdx).NotAssinedIndex;
-    PossibleAnswer(PossibleAnswerCounter).Norm = TempPossibleAnswer(minIdx).Norm;
-    PossibleAnswerCounter = PossibleAnswerCounter + 1;
 
+    [Cmin,minIdx] = min([PossibleAnswer.Norm]);
+    a = PossibleAnswer(minIdx);
+    imageCell{a.rowInFoundImage,a.ColumnInFoundImage} = NotAssigned(a.NotAssinedIndex).img;
+    
+    FoundImage(end+1).row =  a.rowInFoundImage;
+    FoundImage(end).Column =  a.ColumnInFoundImage;
+    FoundImage(end).Top =  NotAssigned(a.NotAssinedIndex).Top;
+    FoundImage(end).Bottom =  NotAssigned(a.NotAssinedIndex).Bottom;
+    FoundImage(end).Left =  NotAssigned(a.NotAssinedIndex).Left;
+    FoundImage(end).Right =  NotAssigned(a.NotAssinedIndex).Right;
+    
+    NotAssigned(a.NotAssinedIndex) = []; % Since we assigned this index, we can remove it from not assigned array
+    notAssignedBottom = {NotAssigned.Bottom};
+    notAssignedTop = {NotAssigned.Top};
+    notAssignedLeft = {NotAssigned.Left};
+    notAssignedRight = {NotAssigned.Right};
+    out = CellToImage(imageCell);
+    imshow(out)
+    %pause;
 end
 
-[Cmin,minIdx] = min([PossibleAnswer.Norm]);
 
-
-out = CellToImage(imageCell);
-figure; imshow(out)
-
-pic = CellToImage(imageCell);
-TopLeft = imageCell{1,1};
-TopRight = imageCell{1,lastColumn};
-
-BottomLeft = imageCell{lastRow,1};
-BottomRight = imageCell{lastRow,lastColumn};
-TopRight = imageCell{1,lastColumn};
-
-CropedBottomRight = TopRight(:,1: size(TopRight,2) * 0.1 ,:);
-[featureVector,hogVisualization] = extractHOGFeatures(CropedBottomRight);
-
-figure;
-imshow(CropedBottomRight); 
-hold on;
-plot(hogVisualization);
-
-
-for i = 1: 36
-    TestImage = Dataset{i,2};
-    new = cat(2,TestImage,TopRight);
-    CropedTestImage = TestImage(:,size(TestImage,2) * 0.9:end,:);
-    [TestfeatureVector,TesthogVisualization] = extractHOGFeatures(CropedTestImage);
-    
-    %figure;
-    %imshow(CropedTestImage); 
-    %hold on;
-    %plot(TesthogVisualization);
-
-    %figure;
-    %imshow(TestImage);
-    n(i) = norm(featureVector - TestfeatureVector);
-
-end
-n
-TestImage = Dataset{1,2};
-new = cat(2,TestImage,TopRight);
-
-figure;
-imshow(new);
-
-
-
-
-%BW1 =  edge(rgb2gray(BottomRight),'Canny');\
-%BW2 = edge(rgb2gray(new),'Canny');
-%[BGmag,BGdir] = imgradient(rgb2gray(BottomRight),'intermediate');
-%[NGmag,NGdir] = imgradient(rgb2gray(Dataset{18,2}),'intermediate');
-%figure; imshow(BGdir ,[]);
-%figure; imshow(NGdir,[]);
--- Assignment4/FindHogForEdges.m
+++ Assignment4/FindHogForEdges.m
@@ -1,18 +1,54 @@
 function [Top,Left,Right,Bottom] = FindHogForEdges(img)
+    
 
-    Croped = img(1:size(img,1) * 0.1,:,:); %Croped Top
-    features = extractHOGFeatures(Croped);
+    numOfPixle = 3;
+    Croped = img(1:numOfPixle,:,:); %Croped Top
+    
+    R=single(transpose(imhist(Croped(:,:,1))));
+    G=single(transpose(imhist(Croped(:,:,2))));
+    B=single(transpose(imhist(Croped(:,:,3))));
+    
+    features1 = extractHOGFeatures(Croped);
+    features2 = extractLBPFeatures(rgb2gray(Croped));
+    features = [features1 features2 R G B single(transpose(Croped(:)))];
     Top =  features;
 
-    Croped = img(size(img,1) * 0.9:end,:,:); %Croped Bottom
-    features = extractHOGFeatures(Croped);
+    Croped = img(size(img,1) - numOfPixle +1 :end,:,:); %Croped Bottom
+    
+    
+    R=single(transpose(imhist(Croped(:,:,1))));
+    G=single(transpose(imhist(Croped(:,:,2))));
+    B=single(transpose(imhist(Croped(:,:,3))));
+    
+    features1 = extractHOGFeatures(Croped);
+    features2 = extractLBPFeatures(rgb2gray(Croped));
+    features = [features1 features2 R G B single(transpose(Croped(:)))];
+    
     Bottom =  features;
 
-    Croped = img(:,size(img,2) * 0.9:end,:); %Croped Left
-    features = extractHOGFeatures(Croped);
-    Left =  features;
-
-    Croped = img(:,1:size(img,2) * 0.1,:); %Croped Right
-    features = extractHOGFeatures(Croped);
+    Croped = img(:,size(img,2) - numOfPixle +1:end,:); %Croped Right
+   
+    
+    R=single(transpose(imhist(Croped(:,:,1))));
+    G=single(transpose(imhist(Croped(:,:,2))));
+    B=single(transpose(imhist(Croped(:,:,3))));
+    
+    features1 = extractHOGFeatures(Croped);
+    features2 = extractLBPFeatures(rgb2gray(Croped));
+    features = [features1 features2 R G B single(transpose(Croped(:)))];
+    
     Right =  features;
+
+    Croped = img(:,1:numOfPixle,:); %Croped Left
+    
+    
+    R=single(transpose(imhist(Croped(:,:,1))));
+    G=single(transpose(imhist(Croped(:,:,2))));
+    B=single(transpose(imhist(Croped(:,:,3))));
+    
+    features1 = extractHOGFeatures(Croped);
+    features2 = extractLBPFeatures(rgb2gray(Croped));
+    features = [features1 features2 R G B single(transpose(Croped(:)))];
+    
+    Left =  features;
 end
